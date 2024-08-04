f74c1574335779975ad2fca4656b3a9699f641e3
Surendra Sharma <103622956+nittsurendra@users.noreply.github.com>
2024-08-04 17:02:41 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://full-stack-food-delivery-web-application.onrender.com'
+
 export const assets ={
     logo,
     add_icon,
@@ -13,5 +15,3 @@ export const assets ={
     upload_area,
     parcel_icon
 }
-
-export const url = 'http://localhost:4000'
